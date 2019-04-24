914cc8a80caf73692efee200d2cfc84324172876
Rupert Nash <r.nash@epcc.ed.ac.uk>
2019-04-24 10:48:12 +0100

    Rewrite how polnet helper deals with paths

--- tests/run_hemelb.py
+++ tests/run_hemelb.py
@@ -78,12 +78,11 @@ def count_sites(gmy_filename):
 
     return total_fluid
 
-def get_gmy_filename_from_xml(xml_filename):
+def get_gmy_filename_from_xml(xml_path):
+    """Return GMY file as written in XML (i.e. rel to XML)"""
     from xml.etree import ElementTree
-    tree = ElementTree.parse(xml_filename)
-    gmy_filename = tree.getroot().find('geometry/datafile').attrib['path']
-    xml_dir = os.path.dirname(os.path.abspath(xml_filename))
-    return os.path.normpath(os.path.join(xml_dir, gmy_filename))
+    tree = ElementTree.parse(xml_path)
+    return tree.getroot().find('geometry/datafile').attrib['path']
 
 def download_file(JOBS_URL, job_id, filename, output_dir, session):
     file_url = JOBS_URL + "/" + job_id + "/files/" + filename
@@ -99,10 +98,17 @@ def download_file(JOBS_URL, job_id, filename, output_dir, session):
                 f.write(chunk)
 
 
-def submit_and_fetch_simulation(conf, xml_file, gmy_file, output_dir):
+def submit_and_fetch_simulation(conf, xml, output_dir):
     '''Return True on successful execution and fetch results, and False or exception on failure'''
-    with requests.Session() as s:
+    xml_path_abs = os.path.abspath(xml)
+    input_dir = os.path.dirname(xml_path_abs)
+    gmy_file_name = os.path.normpath(get_gmy_filename_from_xml(xml_path_abs))
+    xml_file_name = os.path.basename(xml_path_abs)
+    gmy_path_abs = os.path.normpath(os.path.join(input_dir, gmy_file_name))
 
+    assert gmy_file_name == os.path.basename(gmy_path_abs), "Path issues"
+
+    with requests.Session() as s:
 
         login_credentials = {
             'email': conf['username'],
@@ -119,11 +125,11 @@ def submit_and_fetch_simulation(conf, xml_file, gmy_file, output_dir):
         assert p.status_code == 200
 
         # create a new job using the template name
-        n_sites = count_sites(gmy_file)
+        n_sites = count_sites(gmy_path_abs)
         template_name = Cirrus.choose_template(n_sites)
         payload = {}
         payload['template_name'] = template_name
-        payload['arguments'] = xml_file
+        payload['arguments'] = xml_file_name
 
         # Post the job spec, job is created with NEW state
         p = s.post(JOBS_URL, json=payload)
@@ -134,7 +140,7 @@ def submit_and_fetch_simulation(conf, xml_file, gmy_file, output_dir):
 
         # upload the input files
         file_url = JOBS_URL + "/" + str(job_id) + "/files"
-        input_files = {xml_file: open(xml_file, 'rb'), gmy_file: open(gmy_file, 'rb')}
+        input_files = {xml_file_name: open(xml_path_abs, 'rb'), gmy_file_name: open(gmy_path_abs, 'rb')}
         p = s.post(file_url, files=input_files)
         assert p.status_code == 200
 
@@ -188,22 +194,19 @@ def submit_and_fetch_simulation(conf, xml_file, gmy_file, output_dir):
 if __name__ == '__main__':
     with warnings.catch_warnings() as w:
         warnings.simplefilter("ignore")
+
         parser = argparse.ArgumentParser(description='Submit a HemeLB simulation')
         parser.add_argument('xml_file', help='HemeLB XML input file')
-        #parser.add_argument('template_name', help='template name')
         parser.add_argument('conf_file', help='Configuration file')
+
         args = parser.parse_args()
         xml_file = args.xml_file
         conf_file = args.conf_file
-        #template_name = None if args.template_name.lower() == 'none' else args.template_name
         output_dir = os.getcwd()
-        gmy_file = get_gmy_filename_from_xml(xml_file)
 
-        conf = None
         with open(conf_file, 'r') as f:
             conf = json.load(f)
 
-        ok = submit_and_fetch_simulation(conf, xml_file, gmy_file, output_dir)
+        ok = submit_and_fetch_simulation(conf, xml_file, output_dir)
 
         sys.exit(0 if ok else 1)
-
